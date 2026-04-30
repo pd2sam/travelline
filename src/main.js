559015e0e2b7dd552ae90e51c14d13c170a30d3b
pd2sam <pdsam@bk.ru>
2026-04-30 14:14:08 +0300

    ещё форматнула

--- src/main.js
+++ src/main.js
@@ -1,39 +1,39 @@
-const cards = document.querySelectorAll('.hotel-card');
+const cards = document.querySelectorAll(".hotel-card");
 
-cards.forEach(card => {
-    const checkbox = card.querySelector('.hotel-card__checkbox');
-    const btn = card.querySelector('.hotel-card__button');
-    const paymentLink = card.querySelector('.payment-link');
+cards.forEach((card) => {
+  const checkbox = card.querySelector(".hotel-card__checkbox");
+  const btn = card.querySelector(".hotel-card__button");
+  const paymentLink = card.querySelector(".payment-link");
 
-    btn.addEventListener('click', (e) => {
-        e.stopPropagation();
-        if (!card.classList.contains('hotel-card--reserved')) {
-            checkbox.checked = true;
-            card.classList.add('hotel-card--selected');
-        }
-    });
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (!card.classList.contains("hotel-card--reserved")) {
+      checkbox.checked = true;
+      card.classList.add("hotel-card--selected");
+    }
+  });
 
-    paymentLink.addEventListener('click', (e) => {
-        e.stopPropagation();
-    });
+  paymentLink.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
 
-    card.addEventListener('mouseleave', () => {
-        if (card.classList.contains('hotel-card--selected')) {
-            card.classList.remove('hotel-card--selected');
-            card.classList.add('hotel-card--reserved');
-        }
-    });
+  card.addEventListener("mouseleave", () => {
+    if (card.classList.contains("hotel-card--selected")) {
+      card.classList.remove("hotel-card--selected");
+      card.classList.add("hotel-card--reserved");
+    }
+  });
 
-    card.addEventListener('click', () => {
-        if (card.classList.contains('hotel-card--reserved')) {
-            card.classList.remove('hotel-card--reserved');
-            checkbox.checked = false;
-        }
-    });
+  card.addEventListener("click", () => {
+    if (card.classList.contains("hotel-card--reserved")) {
+      card.classList.remove("hotel-card--reserved");
+      checkbox.checked = false;
+    }
+  });
 
-    const nameLink = card.querySelector('.hotel-card__name-link');
+  const nameLink = card.querySelector(".hotel-card__name-link");
 
-    nameLink.addEventListener('click', (e) => {
-        e.stopPropagation();
-    });
+  nameLink.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
 });
